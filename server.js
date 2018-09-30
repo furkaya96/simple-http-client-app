6381e0d0251a4e6be1d7ee0ca0813cecbac57206
furkaya06 <furkaya06@gmail.com>
2018-09-30 21:36:51 +0300

    app name added server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/simple-http-client-app'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/simple-http-client-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
